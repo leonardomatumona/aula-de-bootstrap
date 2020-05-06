c1bcaef6646fe92a56afac435a2e289dd2d525c8
leonardomatumona <leonardomatumona@gmail.com>
2020-05-06 00:49:48 -0300

    exercicio

--- java.js
+++ java.js
@@ -2,18 +2,13 @@ let nome = "Leonardo";
 let idade = 24
 const aluno = 'true';
 
-function inicializaçao() {
-   
-    console.log('carregou a pagina',)
-    console.log('meu nome é:', nome)
-    console.log('minha idade é', idade)
-    console.log('eu sou aluno',aluno)
+function popularParagrafo(){
+    const p = document.querySelector("#primeiro");
+    p.textContent= nome;
+    const pp =document.querySelector ("#segundo")
+    pp.textContent= idade
+    const ppp= document.querySelector("#terceiro")
+    ppp.textContent= aluno
 }
-function processamento(){
 
-    let sobrenome="Matumona"
-    console.log('Meu nome completo é:',nome,sobrenome)
-    console.log('daqui a 10 anos terei:',idade+10)
-}
-inicializaçao()
-processamento()
+popularParagrafo()
