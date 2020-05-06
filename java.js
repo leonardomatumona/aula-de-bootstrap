719df82af24732596f7a1dacfc1f35ef005dcf5a
leonardomatumona <leonardomatumona@gmail.com>
2020-05-06 00:24:02 -0300

    usando javascript externa

---
+++ java.js
@@ -0,0 +1,19 @@
+let nome = "Leonardo";
+let idade = 24
+const aluno = 'true';
+
+function inicializaçao() {
+   
+    console.log('carregou a pagina',)
+    console.log('meu nome é:', nome)
+    console.log('minha idade é', idade)
+    console.log('eu sou aluno',aluno)
+}
+function processamento(){
+
+    let sobrenome="Matumona"
+    console.log('Meu nome completo é:',nome,sobrenome)
+    console.log('daqui a 10 anos terei:',idade+10)
+}
+inicializaçao()
+processamento()
