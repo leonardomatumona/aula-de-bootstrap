2715a485ca3f938bdb1f8e7082d64a26217079e2
leonardomatumona <leonardomatumona@gmail.com>
2020-05-08 20:33:27 -0300

    continuação

---
+++ exerciciolamp.js
@@ -0,0 +1,50 @@
+//function ligar() {
+  //  const lampada = document.querySelector("#lampada");
+ //   lampada.src = "img lampada/lampada-ligada.jpg"
+  //  lampada.alt = "lampada ligado"
+
+  //  const botaoligado = document.querySelector("#botao-ligar")
+  //  botaoligado.disabled = true
+
+ //   const botaodesligar = document.querySelector("#botao-desligar")
+ //   botaodesligar.disabled = false
+//}
+//function desligar() {
+  //  const lampada = document.querySelector("#lampada");
+   // lampada.src = "img lampada/lampada-desligada.jpg"
+   // lampada.alt = "lampada desligada"
+  
+   // const botaoligado = document.querySelector("#botao-ligar")
+  //  botaoligado.disabled = false
+   
+   // const botaodesligar = document.querySelector("#botao-desligar")
+  //  botaodesligar.disabled = true
+
+//}
+//function alternar(){
+ //   if( ligando== true){
+  //      ligar();
+//
+   // } else {
+  //      desligar();
+
+  //  }
+//}
+function interruptor(botao){
+    console.log(button)
+    console.log(button.value)
+    console.log(button.textContent)
+
+    if (button.value ==="ligar") {
+        const lampada = document.querySelector("lampada")
+        lampada.src="img lampada/lampada-ligada.jpg"
+        lampada.textContent= 
+        
+
+
+        
+    } else {
+        
+    }
+    
+}
